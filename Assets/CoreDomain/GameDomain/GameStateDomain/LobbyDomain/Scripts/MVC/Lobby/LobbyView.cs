4dca205b9b41440e4fbd6dca4ff9401563f24117
dextrux <titolucas1@live.com>
2026-09-20 09:56:06 -0300

    feature-GymServerAndClient

--- Assets/CoreDomain/GameDomain/GameStateDomain/LobbyDomain/Scripts/MVC/Lobby/LobbyView.cs
+++ Assets/CoreDomain/GameDomain/GameStateDomain/LobbyDomain/Scripts/MVC/Lobby/LobbyView.cs
@@ -1,19 +1,34 @@
 using CoreDomain.Scripts.Services.SceneService;
 using System;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LobbyView : MonoBehaviour
 {
     [SerializeField] private Button _zooButton;
+    [SerializeField] private Button _GYMClientButton;
+    [SerializeField] private Button _GYMServerButton;
 
-    private Action<string> OnClickZooButton;
-    public void SetUp(Action<string> OnZooButonCLick) {
-        OnClickZooButton = OnZooButonCLick;
+    private Action<string> OnClickButton;
+    public void SetUp(Action<string> OnButtonCLick) {
+        OnClickButton = OnButtonCLick;
         _zooButton.onClick.AddListener(OnZooClick);
+        _GYMClientButton.onClick.AddListener(OnGYMClientClick);
+        _GYMServerButton.onClick.AddListener(OnGYMServerClick);
     }
 
     private void OnZooClick() {
-        OnClickZooButton.Invoke("ZooScene");
+        OnClickButton.Invoke("ZooScene");
+    }
+
+    private void OnGYMServerClick() {
+        OnClickButton.Invoke("GYMScene");
+        NetworkManager.Singleton.StartHost();
+    }
+
+    private void OnGYMClientClick() {
+        OnClickButton.Invoke("GYMScene");
+        NetworkManager.Singleton.StartClient();
     }
 }
